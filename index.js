89abc7779831ff5198f6a430067214cc3de96649
jbibeau96 <jbibeau96@gmail.com>
2022-10-08 02:17:24 -0700

    First commit in this Dir

---
+++ index.js
@@ -0,0 +1,24 @@
+let bankAccountBalance = 52.1;
+
+let firstName = "Tom";
+let lastName = "Sawyer";
+
+let numberOfFriends = 5;
+
+let isHotOutside = false;
+let isHappy = true;
+
+console.log("Hello World!");
+
+console.log(5 + 3);
+console.log(4 - 2);
+console.log(6 * 7);
+console.log(8 / 2);
+
+console.log(bankAccountBalance - 30);
+console.log(bankAccountBalance * numberOfFriends);
+
+console.log(5 == 5);
+console.log(5 == 6);
+console.log(bankAccountBalance == numberOfFriends);
+console.log(bankAccountBalance == 52.1);
